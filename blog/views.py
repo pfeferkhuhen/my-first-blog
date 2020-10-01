71d6364076a0521444aab229fbe2767169187016
pfeferkhuhen <avolkova.editor@gmail.com>
2020-10-01 09:47:30 +0300

    Post_list view edited

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date')[:10]
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()
+        ).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
